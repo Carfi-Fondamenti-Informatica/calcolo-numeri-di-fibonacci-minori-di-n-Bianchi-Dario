30334c2687b0a88e3b7f104d4a9cd429d582b9fc
Bianchi-Dario <91790218+Bianchi-Dario@users.noreply.github.com>
2021-11-14 13:12:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main () {
+    int i, n, primo=1, secondo=1, terzo=0;
+    cin>>n;
+    for(i=1;i<n;i++){
+        terzo=primo+secondo;
+        primo=secondo;
+        secondo=terzo;
+        cout<<terzo<<endl;
+    }
+    return 0;
 }
